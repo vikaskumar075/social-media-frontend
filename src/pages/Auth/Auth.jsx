6911d50d8662df97ad5ddb01b5dbbd23fd28bf43
vikaskumar017 <vikaskumar638727@gmail.com>
2023-09-06 14:52:24 +0530

    first commit

---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Auth.css";
+import Logo from "../../img/logo.png";
+
+const Auth = () => {
+  return (
+    <div className="Auth">
+      <div className="a-left">
+        <img src={Logo} alt="" />
+        <div className="Webname">
+          <h1>ZKC Media</h1>
+          <h6>Explore the ideas throughout the world</h6>
+        </div>
+      </div>
+
+      <LogIn/>
+    </div>
+  );
+};
+function LogIn() {
+    return (
+      <div className="a-right">
+        <form className="infoForm authForm">
+          <h3>Log In</h3>
+  
+          <div>
+            <input
+              type="text"
+              placeholder="Username"
+              className="infoInput"
+              name="username"
+            />
+          </div>
+  
+          <div>
+            <input
+              type="password"
+              className="infoInput"
+              placeholder="Password"
+              name="password"
+            />
+          </div>
+  
+          <div>
+              <span style={{ fontSize: "12px" }}>
+                Don't have an account Sign up
+              </span>
+            <button className="button infoButton">Login</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+
+export default Auth;
